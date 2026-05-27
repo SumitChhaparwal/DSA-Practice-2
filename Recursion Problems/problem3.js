ea39fb5dce06412965d67eb1c6b78d0da29f965a
SumitChaparwal <chhaparwalsumit33@gmail.com>
2026-05-27 12:18:23 +0530

    sum of n numbers

---
+++ Recursion Problems/problem3.js
@@ -0,0 +1,11 @@
+//calculating sum of numbers from 1 to n.. using recursion.
+
+function sumOfNum(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n + sumOfNum(n - 1);
+}
+
+let sum = sumOfNum(5);
+console.log("Sum of numbers:",sum);
