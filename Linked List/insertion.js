5872cefcff3b0e0ba966e564664df321fbbec46f
SumitChaparwal <chhaparwalsumit33@gmail.com>
2026-05-27 17:41:49 +0530

    insertion in linkedlist

---
+++ Linked List/insertion.js
@@ -0,0 +1,69 @@
+//Insertion in linked list ..
+// Types of Insetion:
+// * at beginning of LL
+// * at end of LL
+// * at specific location of LL
+
+//creating linked list
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+let headNode = new Node(5);
+let firstNode = new Node(2);
+let secondNode = new Node(1);
+let thirdNode = new Node(8);
+
+headNode.next = firstNode;
+firstNode.next = secondNode;
+secondNode.next = thirdNode;
+
+
+//*Inserting node at beginning of Linkedlist..
+function insertAtB(val) {
+  let newNode = new Node(val);
+  newNode.next = headNode;
+  headNode = newNode;
+}
+insertAtB(23);
+insertAtB(6);
+//Time Complexity: O(1)
+//Space Complexity: O(1)
+
+
+//*Inserting node at specific position in Linkedlist..
+function insertAtSpecificPosition(previousNode, val) {
+  let newNode = new Node(val);
+  newNode.next = previousNode.next;
+  previousNode.next = newNode;
+}
+insertAtSpecificPosition(firstNode, 21);
+//Time Complexity: O(1)
+//Space Complexity: O(1)
+
+
+//*Inserting node at end of linkedlist..
+function insertAtEnd(val) {
+  let newNode = new Node(val);
+  let pointer = headNode;
+  while(pointer.next !== null){
+    pointer = pointer.next;
+  }
+  pointer.next = newNode;
+}
+insertAtEnd(15);
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+
+
+//displaying updates
+function displayU(head) {
+  let pointer = head;
+  while (pointer !== null) {
+    console.log(pointer.data);
+    pointer = pointer.next;
+  }
+}
+displayU(headNode);
