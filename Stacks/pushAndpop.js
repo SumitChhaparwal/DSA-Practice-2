d1a6b115ad90beefcb100169f7da1476216d8f60
SumitChaparwal <chhaparwalsumit33@gmail.com>
2026-05-28 20:52:51 +0530

    push and pop in stacks

---
+++ Stacks/pushAndpop.js
@@ -0,0 +1,43 @@
+//Adding and Removing elements in stacks using array(Array implementation of stacks)
+let arr = new Array(6);
+let length = arr.length - 1;
+let top = -1;
+
+//funtion to add a element in stacks
+function push(data) {
+  top++;
+  if (top > length) {
+    console.log(`Data cannot be add due to stack overflow..`);
+    top--;
+    return;
+  }
+  arr[top] = data;
+}
+push(2);
+push(4);
+push(6);
+push(1);
+push(6);
+push(11);
+
+//function to remove a element in stacks
+let x =0;
+function pop() {
+  if(top < 0){
+    console.log("stack is empty..");
+    return false;
+  }
+  x = arr[top];
+  top--;
+  return x;
+}
+pop();
+pop();
+pop();
+
+function traverse() {
+  for (let i = top; i >= 0; i--) {
+    console.log(arr[i]);
+  }
+}
+traverse();
