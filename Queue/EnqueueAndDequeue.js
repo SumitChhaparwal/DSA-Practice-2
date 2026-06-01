bf048c061a377f67064647fd489945daf2617ed3
SumitChaparwal <chhaparwalsumit33@gmail.com>
2026-06-01 19:48:42 +0530

    Implemented Dequeue

--- Queue/EnqueueAndDequeue.js
+++ Queue/EnqueueAndDequeue.js
@@ -8,28 +8,46 @@ class Queue {
     this.rear = -1;
   }
 
-  enqueue(data){
-    if(this.rear === this.size-1){
+  enqueue(data) {
+    if (this.rear === this.size - 1) {
       console.log("Queue is full so you cannot add new element..");
       return;
     }
-    if(this.front === -1){
+    if (this.front === -1) {
       this.front = 0;
     }
     this.rear++;
     this.qArray[this.rear] = data;
   }
 
-  displayValue(){
-    if(this.front === -1 && this.rear === -1){
-      console.log("Queue is empty!");
+  dequeue() {
+    if(this.isEmpty()){
+      console.log("Cannot dequeue element as queue is empty..");
       return;
     }
-    for(let i=this.front; i<=this.rear; i++){
-      console.log(this.qArray[i]);
+    for(let i=0; i<=this.rear; i++){
+      this.qArray[i] = this.qArray[i+1];
+    }
+    this.rear--;
+  }
+
+  isEmpty() {
+    if(this.rear === -1){
+      return true;
+    } else{
+      return false;
     }
   }
 
+  displayValue() {
+    if (this.rear === -1) {
+      console.log("Queue is empty!");
+      return;
+    }
+    for (let i = this.front; i <= this.rear; i++) {
+      console.log(this.qArray[i]);
+    }
+  }
 }
 
 let q1 = new Queue(5);
@@ -38,12 +56,12 @@ q1.enqueue(12);
 q1.enqueue(13);
 q1.enqueue(14);
 q1.enqueue(15);
-q1.enqueue(12);
-q1.enqueue(12);
 //Time Complexity: O(1)
 //Space Complexity: O(1)
 
-
+q1.dequeue();
+q1.dequeue();
+//Time Complexity: O(n)
+//Space Complexity: O(1)
 
 q1.displayValue();
-
