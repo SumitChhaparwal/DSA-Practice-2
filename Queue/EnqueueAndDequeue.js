4166400368035dd47a8ed89c648d394c5c8f0232
SumitChaparwal <chhaparwalsumit33@gmail.com>
2026-06-01 20:21:45 +0530

    Implemented all Queue operations..

--- Queue/EnqueueAndDequeue.js
+++ Queue/EnqueueAndDequeue.js
@@ -21,22 +21,44 @@ class Queue {
   }
 
   dequeue() {
-    if(this.isEmpty()){
+    if (this.isEmpty()) {
       console.log("Cannot dequeue element as queue is empty..");
       return;
     }
-    for(let i=0; i<=this.rear; i++){
-      this.qArray[i] = this.qArray[i+1];
+    for (let i = 0; i <= this.rear; i++) {
+      this.qArray[i] = this.qArray[i + 1];
     }
     this.rear--;
   }
 
+  isFull() {
+    if (this.isEmpty()) {
+      console.log("Queue is empty.");
+      return;
+    }
+    return "Queue is full..";
+  }
+
   isEmpty() {
-    if(this.rear === -1){
+    if (this.rear === -1) {
       return true;
-    } else{
+    } else {
       return false;
     }
+  }
+
+  findPeek() {
+    if (this.rear == -1) {
+      return;
+    }
+    return this.qArray[this.front];
+  }
+
+  findRear() {
+    if (this.rear == -1) {
+      return;
+    }
+    return this.qArray[this.rear];
   }
 
   displayValue() {
@@ -51,6 +73,7 @@ class Queue {
 }
 
 let q1 = new Queue(5);
+//Enqueue
 q1.enqueue(11);
 q1.enqueue(12);
 q1.enqueue(13);
@@ -59,9 +82,18 @@ q1.enqueue(15);
 //Time Complexity: O(1)
 //Space Complexity: O(1)
 
+//Dequeue
 q1.dequeue();
 q1.dequeue();
 //Time Complexity: O(n)
+//Space Complexity: O(1)
+
+//All Queue operations like isFull, isEmpty, findPeek, findRear;
+console.log("queue is full or not: ", q1.isFull());
+console.log("queue is empty or not: ", q1.isEmpty());
+console.log("peek element of queue: ", q1.findPeek());
+console.log("rear element of queue: ", q1.findRear());
+//Time Complexity: O(1)
 //Space Complexity: O(1)
 
 q1.displayValue();
